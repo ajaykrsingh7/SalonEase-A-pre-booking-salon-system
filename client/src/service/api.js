96e2c67f4d714028bc1cffcec1eb0b631571f392
Ajay Kumar Singh <ajaykrsingh2002@gmail.com>
2025-09-16 01:29:59 +0530

    "change api.js"

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,8 +1,16 @@
 import axios from "axios";
 
+// const API = axios.create({
+//   baseURL: `${import.meta.env.VITE_API_URL}/api`,
+// });
+
 const API = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api"
+      : "https://salonease-a-pre-booking-salon-system.onrender.com/api",
 });
+
 
 export const registerUser = async (data) => {
   return await API.post("/register/user", data);
