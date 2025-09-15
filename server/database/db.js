20e30854d07ac89976103fe2e0530b7bd544699a
Ajay Kumar Singh <ajaykrsingh2002@gmail.com>
2025-09-16 02:16:27 +0530

    "update db"

--- server/database/db.js
+++ server/database/db.js
@@ -2,18 +2,11 @@ const mongoose = require('mongoose');
 
 const Connection = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(process.env.MONGO_URI); // options removed
     console.log("DB Connected");
   } catch (error) {
-    console.log("DB Connection Error", error);
+    console.log("DB Connection Error:", error);
   }
 };
 
 module.exports = Connection;
-
-
-
-
